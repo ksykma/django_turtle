c766038918c2c8df43593844b975a3a6e65b531a
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-09-27 16:13:33 +0900

    Turtle 1day study

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def dinner(request, name):
+    menus = [{"name":'족발',"price":30000}, 
+             {"name":'치킨', "price":25000}, 
+             {"name":'마라탕', "price":15000}, 
+             {"name":'떡볶이', "price":5000}, 
+             {"name":'부대찌개', "price":20000}]
+    pick = random.choice(menus)
+    context = {
+        'pick':pick,
+        'name':name,
+        'menus':menus,
+    }
+    return render(request, 'dinner.html', context)
